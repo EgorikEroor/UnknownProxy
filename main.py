552a929f479d245b86a546e9f032159ad377e18e
EgorikEroor <business17293@gmail.com>
2026-07-09 20:48:26 +0300

    Update 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 async def listen_socket_data(reader:asyncio.StreamReader,writer:asyncio.StreamWriter):
-    addres = writer.get_extra_info(name='peername')
+    address = writer.get_extra_info(name = 'peername')
     client_data_request = b''
     while True:
         client_data_getting_request = await reader.read(4096)
@@ -10,8 +10,11 @@ async def listen_socket_data(reader:asyncio.StreamReader,writer:asyncio.StreamWr
         client_data_request += client_data_getting_request
         if b'\r\n\r\n' in client_data_request:
             break
-    print(client_data_request)
-
+    client_data_request = client_data_request.decode('utf-8').strip().split(sep = '\r\n')
+    request_information = {}
+    request_information['Request_method'],request_information['Request_address'],_ = client_data_request[0].split(sep = ' ',maxsplit = 2)
+    request_information['Request_host'],request_information['Request_port'] = request_information['Request_address'].split(sep = ':',maxsplit= 1)
+    server_socket = await asyncio.open_connection(host=request_information['Request_host'],port=request_information['Request_port'])
 async def run_unknown_proxy():
     listen_socket = await asyncio.start_server(client_connected_cb=listen_socket_data,host='0.0.0.0',port='8080')
     await listen_socket.serve_forever()
