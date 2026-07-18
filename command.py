58544f5c8d7e884b6921fc2bce09269ab1d07e51
EgorikEroor <business17293@gmail.com>
2026-07-18 12:00:40 +0300

    Update 12

--- command.py
+++ command.py
@@ -2,15 +2,23 @@ import asyncio
 from pathlib import Path
 import string
 
-async def found_openssl_directory(path_for_check_exist_rusted_certificate_directory = None):
-    needed_files_for_certification_center = ['root_for_certification_center.key','root_for_certification_center.crt','config_for_certification_center.txt']
+async def check_before_create_certificate(path_for_check_file_in_trusted_certificate_directory = None):
+    inspection_results = {}
+    if path_for_check_file_in_trusted_certificate_directory:
+        needed_files_for_certification_center = ['key_for_certification_center.key','certificate_for_certification_center.crt','config_for_certification_center.txt']
+        for file in needed_files_for_certification_center:
+            if not Path((str(path_for_check_file_in_trusted_certificate_directory) + fr'\{file}')).is_file():
+                inspection_results['Check_file'] = False
+                break
+            else:
+                inspection_results['Check_file'] = True
     for disk_letter in list(string.ascii_uppercase):
         if Path(fr'{disk_letter}:\Program Files\OpenSSL-Win64\bin').is_dir():
-            return Path(fr'{disk_letter}:\Program Files\OpenSSL-Win64\bin')
+            inspection_results['Path_to_openssl_directory'] = Path(fr'{disk_letter}:\Program Files\OpenSSL-Win64\bin')
     for disk_letter in list(string.ascii_uppercase):
         if Path(fr'{disk_letter}:\Program Files\OpenSSL-Win32\bin').is_dir():
-            return Path(fr'{disk_letter}:\Program Files\OpenSSL-Win32\bin')
-    return None
+            inspection_results['Path_to_openssl_directory'] = Path(fr'{disk_letter}:\Program Files\OpenSSL-Win32\bin')
+    return inspection_results
 
 async def create_trusted_certificate(to_what_address):
     path_to_saved_trusted_certificate_directory = Path(f'{Path(__file__).resolve().parent}' + r'\Saved_Trusted_Certificate')
@@ -21,11 +29,11 @@ async def create_trusted_certificate(to_what_address):
     if Path(needed_certfile).is_file() and Path(needed_keyfile).is_file():
         return needed_certfile,needed_keyfile
     else:
-        path_to_openssl_directory = await found_openssl_directory()
-        if path_to_openssl_directory:
+        inspection_results = await check_before_create_certificate(path_for_check_file_in_trusted_certificate_directory = path_to_saved_trusted_certificate_directory)
+        if inspection_results['Check_file'] is True and inspection_results['Path_to_openssl_directory'] is not None:
             argument_for_openssl_command = [('genrsa','-out',fr"{path_to_saved_trusted_certificate_directory}\{clean_address}.key",'2048'),('req','-new','-key',fr"{path_to_saved_trusted_certificate_directory}\{clean_address}.key",'-out',fr"{path_to_saved_trusted_certificate_directory}\{clean_address}.csr",'-subj',f'/CN={clean_address}','-config',fr"{path_to_saved_trusted_certificate_directory}\config_for_certification_center.txt"),('x509','-req','-in',fr"{path_to_saved_trusted_certificate_directory}\{clean_address}.csr",'-CA',fr"{path_to_saved_trusted_certificate_directory}\certificate_for_certification_center.crt",'-CAkey',fr"{path_to_saved_trusted_certificate_directory}\key_for_certification_center.key",'-CAcreateserial','-out',fr"{path_to_saved_trusted_certificate_directory}\{clean_address}.crt",'-days','365','-sha256')]
             for command in argument_for_openssl_command:
-                openssl_process = await asyncio.create_subprocess_exec(fr"{path_to_openssl_directory}\openssl.exe",*command)
+                openssl_process = await asyncio.create_subprocess_exec(fr"{inspection_results['Path_to_openssl_directory']}\openssl.exe",*command)
                 await openssl_process.wait()
             return needed_certfile,needed_keyfile
         else:
@@ -35,10 +43,10 @@ async def create_trusted_certificate(to_what_address):
                 powershell_process.stdin.write(command.encode(encoding = 'utf-8'))
                 await powershell_process.stdin.drain()
             await powershell_process.wait()
-            path_to_openssl_directory = await found_openssl_directory()
+            inspection_results = await check_before_create_certificate()
             argument_for_openssl_command = [('genrsa','-out',fr"{path_to_saved_trusted_certificate_directory}\key_for_certification_center.key",'2048'),('req','-x509','-new','-nodes','-key',fr"{path_to_saved_trusted_certificate_directory}\key_for_certification_center.key",'-sha256','-days','36500','-out',fr"{path_to_saved_trusted_certificate_directory}\certificate_for_certification_center.crt",'-subj',fr'/C=US/ST=Washington/L=Washington/O=TUCFCS/CN=Trusted USA Company For Certificate Signatures'),('req','-x509','-new','-nodes','-key',fr"{path_to_saved_trusted_certificate_directory}\key_for_certification_center.key",'-days','36500','-out',fr"{path_to_saved_trusted_certificate_directory}\certificate_for_certification_center.crt",'-config',fr"{path_to_saved_trusted_certificate_directory}\config_for_certification_center.txt",'-subj','/CN=Trusted USA Company For Certificate Signatures')]
             for command in enumerate(argument_for_openssl_command):
-                openssl_process = await asyncio.create_subprocess_exec(fr"{path_to_openssl_directory}\openssl.exe",*command[1])
+                openssl_process = await asyncio.create_subprocess_exec(fr"{inspection_results['Path_to_openssl_directory']}\openssl.exe",*command[1])
                 await openssl_process.wait()
                 if command[0] == 1:
                     with open(file = r'G:\Programming\MyApps\UP\Saved_Trusted_Certificate\config_for_certification_center.txt',mode = 'w',encoding = 'utf-8') as file:
